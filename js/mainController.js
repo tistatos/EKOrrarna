f0a521f103d284a294bd00761700285bc3ea6c89
NaiLi <linnea.nabo@gmail.com>
2016-02-12 20:54:53 +0100

    separated files

---
+++ js/mainController.js
@@ -0,0 +1,16 @@
+// Controller
+angular.module('ekorrarna').controller('mainController', ['$scope', function($scope) {
+  $scope.greeting = 'Hola';
+
+		/*
+		Färger: 
+		Lila - #592473
+		Grön - #366841
+		Rosa - #b10d61
+		Orange - #ce5e1c
+		Gul - #f4df10
+		Blå - #009cd8
+		*/
+		var colors = ['#592473', '#366841', '#b10d61', '#ce5e1c', '#f4df10', '#009cd8'];
+		
+}]);
