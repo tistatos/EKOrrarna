2158b1cd075b0c65f2e66264acfefd5115c2f4f3
tistatos <tistatos@gmail.com>
2016-02-13 13:59:48 +0100

    now arrow looks nice

--- js/databars.js
+++ js/databars.js
@@ -8,7 +8,7 @@ return {
   },
   link: function(scope, element, attrs) {
 
-    var barWidth = 250;
+    var barWidth = 230;
     var oldVal = 0;
     var upTrend = false;
     d3Service.d3().then(function(d3) {
@@ -39,17 +39,13 @@ return {
         dataWidth = Math.max(0,Math.min(dataWidth,barWidth));
         scope.databar.transition().attr("width",dataWidth).duration(2000).ease("linear");
 
-        console.log(oldVal-scope.measurement.data);
-        console.log(upTrend);
         if(oldVal-scope.measurement.data > 0 && upTrend) {
           scope.arrow.transition().duration(800).attrTween("transform",tweenP);
-          console.log("downTrend");
           oldVal = scope.measurement.data;
           upTrend = false;
         }
         else if(oldVal-scope.measurement.data < 0 && !upTrend) {
           upTrend = true;
-          console.log("uptrend");
           scope.arrow.transition().duration(800).attrTween("transform",tween);
           oldVal = scope.measurement.data;
         }
@@ -57,10 +53,10 @@ return {
           oldVal = scope.measurement.data;
         }
         function tweenP(d, i, a) {
-          return d3.interpolateString("rotate(45, 265, 10)", "rotate(135, 265, 10)");
+          return d3.interpolateString("rotate(45, 245, 10)", "rotate(135, 245, 10)");
         }
         function tween(d, i, a) {
-          return d3.interpolateString("rotate(135, 265, 10)", "rotate(45, 265, 10)");
+          return d3.interpolateString("rotate(135, 245, 10)", "rotate(45, 245, 10)");
         }
       }
 
@@ -93,13 +89,13 @@ return {
             .style("fill", "black");
 
 
-          var pts = '265, 0, 270, 10, 267,10, 267, 20 262,20 262,10 260,10 265,0';
+          var pts = '245, 0, 250, 10, 247,10, 247, 20 242,20 242,10 240,10 245,0';
           scope.arrow = svg.append('polyline')
 						.attr("x",0)
             .attr('points', pts)
             .style('stroke', 'white')
 						.attr("y",-100)
-            .attr("transform", "rotate(45, 265, 10)")
+            .attr("transform", "rotate(45, 245, 10)")
             .style('fill', 'white');
 
         }
