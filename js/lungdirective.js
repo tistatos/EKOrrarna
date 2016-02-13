d67cae4abae02428408a4d3c3c332c4c8e64e5ab
NaiLi <linnea.nabo@gmail.com>
2016-02-13 09:13:44 +0100

    lung directive

--- js/lungdirective.js
+++ js/lungdirective.js
@@ -1,75 +1,59 @@
 // lungdirective
 angular.module('ekorrarna')
 .directive('lungdirective', ['d3Service', function(d3Service) {
-return {
-  link: function(scope, element, attrs) {
-		/*
-		Färger: 
-		Lila - #592473
-		Grön - #366841
-		Rosa - #b10d61
-		Orange - #ce5e1c
-		Gul - #f4df10
-		Blå - #009cd8
-		*/
-	  	var pos = [
-	  		{
-	  			x: 0,
-	  			y: 0
-	  		},
-	  		{
-	  			x: 0,
-	  			y: 1
-	  		},
-	  		{
-	  			x: 1,
-	  			y: 0
-	  		},
-	  		{
-	  			x: 1,
-	  			y: 1
-	  		},
-	  	];
 
-    d3Service.d3().then(function(d3) {
-    	
-      var svg = d3.select(element[0])
-        .append('svg')
-        .style('width', '100%');
+	// Lungorna
+  var url = 'data/lung.json';
+  var request = new XMLHttpRequest();
+          request.open("GET", url, false);
+          request.send(null)
+  var pos = JSON.parse(request.responseText);
 
-      // Browser onresize event
-      window.onresize = function() {
-        scope.$apply();
-      };
+  var radius = 7;
+  var circleDistance = radius*2 + 2;
 
-      // hard-code data
-      scope.data = [
-        {name: "Greg", score: 98},
-        {name: "Ari", score: 96},
-        {name: 'Q', score: 75},
-        {name: "Loser", score: 48}
-      ];
+	return {
+		scope: true,
+	  link: function(scope, element, attrs) {
 
-      // Watch for resize event
-      scope.$watch(function() {
-        return angular.element(window)[0].innerWidth;
-      }, function() {
-        scope.render(scope.data);
-      });
+	    d3Service.d3().then(function(d3) {
+	    	
+	      svg = d3.select(element[0])
+	        .append('svg')
+	        .style('width', '40%')
+	        .style('height', '100vw');
+
+	      // Browser onresize event
+	      window.onresize = function() {
+	        scope.$apply();
+	      };
+
+	      // Watch for resize event
+	      scope.$watch(function() {
+	        return angular.element(window)[0].innerWidth;
+	      }, function() {
+	        scope.render(scope.data);
+	      });
+
+	      scope.updateLung = function() {
+
+			  	pos.forEach(item => {
+
+			  		// Number 0-9 (random bland aktuella färger)
+						var rand = Math.floor((Math.random() * 10));
+			  		var mySquare=svg.append("circle")
+						  .attr("cx",(1+item.x)*circleDistance)
+						  .attr("cy",(1+item.y)*circleDistance)
+						  .attr("r",radius)
+						  .style("fill",scope.amountColors[rand]);
+
+			  	})
+				}
+
+	    });
 
       scope.render = function(data) {
-
-      	pos.forEach(item => {
-
-      		var mySquare=svg.append("rect")
-					  .attr("x",item.x*60)
-					  .attr("y",item.y*60)
-					  .attr("width",60)
-					  .attr("height",60)
-					  .style("fill",'#592473');
-      	})
-        // our custom d3 code
+      	scope.updateLung();
       }
-    });
   }};
 }]);
